213b94446e50ab319ed57bb13899c7d7b29b9501
Arka Pal <arka.pal.0521@gmail.com>
2022-04-09 20:58:05 -0700

    more details added

--- WaveTester/WaveTester/Views/ClothDataView.swift
+++ WaveTester/WaveTester/Views/ClothDataView.swift
@@ -5,16 +5,17 @@
 //  Created by Arka Pal on 4/8/22.
 //
 import SwiftUI
-
+import Foundation
 
 struct ClothDataView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var listViewModel:ListViewModel
-    var cloth_entry = Clothes(id:0, R_COLOR:0, G_COLOR:0, B_COLOR:0, TYPE: "N/A",RECENT_DATE_WORN:"N/A", TIMES_WORN: 0,RATING:0,OC_FORMAL:0,OC_SEMI_FORMAL:0,OC_CASUAL:0,OC_WORKOUT:0, OC_OUTDOORS:0, OC_COMFY:0,WE_COLD:0,WE_HOT:0,WE_RAINY:0,WE_SNOWY: 0,WE_AVG_TMP:0)
+
     @State var textFieldText: String = ""
     @State var color_sample: String = ""
+    var ratingOptions = ["1","2","3","4","5","6","7","8","9","10"]
     var typeOptions = ["Shirts", "OverTops", "Pants", "Shorts", "Shoes", "Hat", "Misc"]
-    var shirtOptions = ["Shirts", "Button Up", "Tee", "Polo Shirt", "Graphic", "Athletic", "Long Sleeve Button Up", "Long Sleeve Polo", "Long Sleeve Tee", "Long Sleeve Graphic", "Long Sleeve Athletics", "Flannel "]
+    var shirtOptions = ["Tank Tops", "Button Up", "Tee", "Polo Shirt", "Graphic", "Athletic", "Long Sleeve Button Up", "Long Sleeve Polo", "Long Sleeve Tee", "Long Sleeve Graphic", "Long Sleeve Athletics", "Flannel "]
     var overtopOptions = ["Sweatshirt", "Hoodie", "Jacket",
                           "Blazer", "Coats", "Vest", "Puffer Jacket"]
     var pantOptions = ["Skirt", "Yoga pants", "Cargo Pants", "Jeans", "Sweatpants", "Track Pants", "Dress Pants", "Pajamas"]
@@ -34,7 +35,10 @@ struct ClothDataView: View {
     @State private var cloth = ""
     @State private var ctype = "cloth type"
     @State private var previewIndex = 0
+    @State private var rating = 0
     @State private var color = Color.blue
+    @State private var recent_date = Date()
+    @State private var current_date = Date()
     var body: some View {
         NavigationView {
                     Form {
@@ -107,7 +111,7 @@ struct ClothDataView: View {
                             }
 
                         }
-                        Section(header: Text("\(cloth) Color")) {
+                        Section(header: Text("\(ctype) Color")) {
 //                            Picker(selection: $previewIndex, label: Text("Tap to Choose")) {
 //                                ForEach(0 ..< colorOptions.count) {
 //                                    Text(self.colorOptions[$0])
@@ -118,9 +122,21 @@ struct ClothDataView: View {
                                       .background(color)
 
                         }
+                        Section(header: Text("Most recent date worn")) {
+                            DatePicker("", selection: $recent_date)
+                        }
                         Section(header: Text("Number of Times Worn")) {
                             TextField("Ex: 3", text: $color_sample)
                         }
+                        
+                        Section(header: Text("Choose rating from 1-10(10 being highest rank)")) {
+                            Picker(selection: $rating, label: Text("Tap to Choose")) {
+                                ForEach(0 ..< ratingOptions.count) {
+                                    Text(self.ratingOptions[$0])
+                                }
+                            }
+                        }
+                       
                         Section(header: Text("Occasion")) {
                             Picker(selection: $previewIndex, label: Text("Tap to Choose")) {
                                 ForEach(0 ..< occasionOptions.count) {
@@ -137,11 +153,13 @@ struct ClothDataView: View {
                         }
                         
                         
+                        
 
 
                         
                     }
                     .navigationBarTitle("Item Details")
+            var cloth_entry = Clothes(id:0, R_COLOR:0, G_COLOR:0, B_COLOR:0, TYPE: "N/A",RECENT_DATE_WORN:"N/A", TIMES_WORN: 0,RATING:0,OC_FORMAL:0,OC_SEMI_FORMAL:0,OC_CASUAL:0,OC_WORKOUT:0, OC_OUTDOORS:0, OC_COMFY:0,WE_COLD:0,WE_HOT:0,WE_RAINY:0,WE_SNOWY: 0,WE_AVG_TMP:0)
                    
                                     }
           }
