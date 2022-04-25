0b86d99df453f84d1ece73d37bfc9e7b247e253c
Arka Pal <arka.pal.0521@gmail.com>
2022-04-25 01:18:39 -0700

    started to work on form

--- dhatchi-test/navigation/screens/HomeScreenComponents/AddScreen.js
+++ dhatchi-test/navigation/screens/HomeScreenComponents/AddScreen.js
@@ -1,39 +1,49 @@
 import React, {useState} from 'react';
 import {Picker, Text, StyleSheet, View, TextInput, Button} from 'react-native';
-
-const DetailsScreen = () => {
+import RNPickerSelect from "react-native-picker-select";
+const AddScreen = () => {
   const [currency, setCurrency] = useState('US Dollar');
   return (
     <View style={styles.container}>
-      <Text style={styles.formLabel}> Demo Form </Text>
+      <Text style={styles.formLabel}> Name </Text>
       <View>
-        <TextInput placeholder="Email" style={styles.inputStyle} />
+        <TextInput placeholder="Ex: Bape Hoodie" style={styles.inputStyle} />
+        <Text style={styles.formLabel}> Color </Text>
         <TextInput
           secureTextEntry={true}
-          placeholder="Password"
+          placeholder="Ex: Blue"
           style={styles.inputStyle}
         />
+         <Text style={styles.formLabel}> Cloth Type </Text>
         <Picker
           selectedValue={currency}
           onValueChange={currentCurrency => setCurrency(currentCurrency)}>
-          <Picker.Item label="USD" value="US Dollars" />
-          <Picker.Item label="EUR" value="Euro" />
-          <Picker.Item label="NGN" value="Naira" />
+          <Picker.Item label="Shirts" value="" />
+          <Picker.Item label="OverTops" value="" />
+          <Picker.Item label="Pants" value="" />
+          <Picker.Item label="Shorts" value="" />
+          <Picker.Item label="Shoes" value="" />
+          <Picker.Item label="Hat" value="" />
+          <Picker.Item label="Misc" value="" />
         </Picker>
-        <Text
-          style={{
-            fontSize: 30,
-            color: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          Selected: {currency}
-        </Text>
-        {/* <Button
+
+        {/* <RNPickerSelect
+                onValueChange={(language) => setLanguage(language)}
+                items={[
+                    { label: "JavaScript", value: "JavaScript" },
+                    { label: "TypeScript", value: "TypeScript" },
+                    { label: "Python", value: "Python" },
+                    { label: "Java", value: "Java" },
+                    { label: "C++", value: "C++" },
+                    { label: "C", value: "C" },
+                ]}
+              style={pickerSelectStyles}
+            /> */}
+        { <Button
           title="Submit"
           color="#fff"
           onPress={() => alert('Simple Button pressed')}
-        /> */}
+        /> }
       </View>
     </View>
   );
@@ -71,4 +81,27 @@ const styles = StyleSheet.create({
   },
 });
 
-export default DetailsScreen;
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+      fontSize: 16,
+      paddingVertical: 12,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 4,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+      fontSize: 16,
+      paddingHorizontal: 10,
+      paddingVertical: 8,
+      borderWidth: 0.5,
+      borderColor: 'purple',
+      borderRadius: 8,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+  }
+});
+
+export default AddScreen;
