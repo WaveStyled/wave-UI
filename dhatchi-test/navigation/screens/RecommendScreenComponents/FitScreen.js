d08031274265462eb3f7b6f02a7af1127e1cc679
matt-dell <mdaxner@ucsc.edu>
2022-05-04 17:54:51 -0700

    Small change to calibrate start fetch

--- dhatchi-test/navigation/screens/RecommendScreenComponents/FitScreen.js
+++ dhatchi-test/navigation/screens/RecommendScreenComponents/FitScreen.js
@@ -3,7 +3,7 @@ import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
 import { API, NODEPORT } from "../../../context/API";
 function FitScreen({ navigation }){
     const clothes = ()=> {
-        fetch(`http://${API}:${NODEPORT}/start_calibrate/123/20`, {method: "PUT"})
+        fetch(`http://${API}:${NODEPORT}/start_calibrate/123/20/`, {method: "PUT"})
       .then((response) => {
         if (!response.ok) {
           throw response;
