1ac66238ceec0a4b3841f757641c83b7479459e9
matt-dell <mdaxner@ucsc.edu>
2022-05-05 17:13:37 -0700

    Weather and Occasion now appear in headers of recommend and will update

--- dhatchi-test/navigation/screens/RecommendScreenComponents/FitScreen.js
+++ dhatchi-test/navigation/screens/RecommendScreenComponents/FitScreen.js
@@ -1,22 +1,79 @@
 import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
 import * as React from 'react';
 import { API, NODEPORT } from "../../../context/API";
-function FitScreen({ navigation }){
-    const [fits, setFits] = React.useState(0);
-    const clothes = ()=> {
-        fetch(`http://${API}:${NODEPORT}/start_calibrate/123/20/`, {method: "PUT"})
-      .then((response) => {
-        if (!response.ok) {
-          throw response;
-        }
-        return response.json();
-      })
-      .then((json) => {
-        setFits(json);
-      });
+import { ClothesContext } from "../../../context/AppContext";
+
+function getFits(set) {
+
+    fetch(`http://${API}:${NODEPORT}/start_calibrate/123/5/`, {method: "PUT"})
+    .then((response) => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    })
+    .then((json) => { 
+    
+        set(json);
+
+    });
     }
-    clothes()
-    var counter = 0
+
+function FitScreen({ navigation }){
+    const [fits, setFits] = React.useState([[[],[]],[[],[]]]);
+    const [curWeather, setWeather] = React.useState("r")
+    const [curOccasion, setOccasion] = React.useState("")
+    const [curIds, setIds] = React.useState([])
+    const [curCounter, incrementCounter] = React.useState(0)
+    const a = React.useContext(ClothesContext);
+    
+    const updateHeader =() => {
+        incrementCounter(curCounter + 1)
+        
+            setOccasion(fits[1][curCounter][0])
+            setWeather(fits[1][curCounter][1])
+            setIds(fits[0][curCounter])
+         
+    }
+       
+    React.useEffect(() => {
+        getFits(setFits);
+      }, []);
+    const setHeader = () => {
+        React.useLayoutEffect(() => {
+            navigation.setOptions({
+              headerRight: () => (
+                <Text>hi</Text>
+              ),
+            });
+          });
+        }
+    var counter = 0;
+    
+   //console.log(fits)
+   React.useEffect(() => {
+    setOccasion(fits[1][curCounter][0])
+    setWeather(fits[1][curCounter][1])
+    setIds(fits[0][curCounter])
+  }, []);
+
+   //setWeather(fits[1][counter][0])
+    //setOccasion(fits[1][0][0])
+    //console.log(curWeather)
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+          headerShown: true,
+            headerRight: () => (
+           <Text>{curWeather}</Text>
+          ),
+          headerLeft: () => (
+            <Text>{curOccasion}</Text>
+          ),
+        });
+      });
+    //console.log(a)
+   // var x = a.filter((item)=> item.pieceid == "102")
+    //console.log(x.map(({pieceid})=>(pieceid)))
     return (
         <View
       style={{
@@ -25,7 +82,111 @@ function FitScreen({ navigation }){
         alignItems: "center"
       }}>
       <Text>Hello, world!</Text>
+      
+            <TouchableOpacity style={styles.commandButton} onPress={updateHeader}>
+            <Text style={styles.panelButtonTitle}>Like</Text>
+            </TouchableOpacity>
+            
     </View>
-    );
+    
+);
 }
+
+const styles = StyleSheet.create({
+    container1: {
+      flex: 1,
+      paddingHorizontal: '10%',
+      justifyContent: 'center',
+      backgroundColor : '#dfe3ee',
+      paddingVertical : '8%'
+    },
+    container: {
+      flex: 1,
+    },
+    commandButton: {
+      padding: 10,
+      borderRadius: 20,
+      backgroundColor: '#2874A6',
+      alignItems: 'center',
+      marginTop: 15,
+    },
+    panel: {
+      padding: 20,
+      backgroundColor: '#58D68D',
+      paddingTop: 20,
+      // borderTopLeftRadius: 20,
+      // borderTopRightRadius: 20,
+      // shadowColor: '#000000',
+      // shadowOffset: {width: 0, height: 0},
+      // shadowRadius: 5,
+      // shadowOpacity: 0.4,
+    },
+    header: {
+      backgroundColor: '#E8EAED',
+      shadowColor: '#333333',
+      shadowOffset: {width: -1, height: -3},
+      shadowRadius: 2,
+      shadowOpacity: 0.4,
+      // elevation: 5,
+      paddingTop: 20,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      paddingVertical : '5%'
+    },
+    panelHeader: {
+      alignItems: 'center',
+    },
+    panelHandle: {
+      width: 40,
+      height: 8,
+      borderRadius: 4,
+      backgroundColor: '#00000040',
+      marginBottom: 10,
+    },
+    panelTitle: {
+      fontSize: 27,
+      height: 35,
+    },
+    panelSubtitle: {
+      fontSize: 14,
+      color: 'gray',
+      height: 30,
+      marginBottom: 10,
+    },
+    panelButton: {
+      padding: 13,
+      borderRadius: 10,
+      backgroundColor: '#45B39D',
+      alignItems: 'center',
+      marginVertical: 7,
+    },
+    panelButtonTitle: {
+      fontSize: 17,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    action: {
+      flexDirection: 'row',
+      marginTop: 10,
+      marginBottom: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#f2f2f2',
+      paddingBottom: 5,
+      paddingTop: 20,
+    },
+    actionError: {
+      flexDirection: 'row',
+      marginTop: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#FF0000',
+      paddingBottom: 5,
+    },
+    textInput: {
+      flex: 1,
+      marginTop: Platform.OS === 'ios' ? 0 : -12,
+      paddingLeft: 10,
+      color: '#05375a',
+    },
+    });
+
 export default FitScreen
