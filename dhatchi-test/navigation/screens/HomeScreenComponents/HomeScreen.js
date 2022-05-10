53b6423361a6ce0539479203ee44ca7b78841f2e
Arka Pal <arka.pal.0521@gmail.com>
2022-05-09 21:16:11 -0700

    made type_mapping importable

--- dhatchi-test/navigation/screens/HomeScreenComponents/HomeScreen.js
+++ dhatchi-test/navigation/screens/HomeScreenComponents/HomeScreen.js
@@ -7,66 +7,9 @@ import ClothingItem from "../../../components/ClothingItem";
 import { useRoute } from "@react-navigation/native";
 import { ClothesContext } from "../../../context/AppContext";
 import { API, NODEPORT } from "../../../context/API";
+import type_mapping from "../../../components/type_mapping";
 
-const type_mapping = {
-  //shirts
-  TATS: 'Tank Top',
-  SBUS: 'Button Up',
-  STES: 'Tee',
-  SPOS: 'Polo Shirt',
-  SGRS: 'Graphic',
-  SATS: 'Athletic',
-  LBUS: 'Long Sleeve Button up',
-  LPOS: 'Long Sleeve Polo',
-  LTES: 'Long Sleeve Tee',
-  LGRS: 'Long Sleeve Graphic',
-  LATS: 'Long Sleeve Athletics',
-  FLAS: 'Flannel',
-  JERSX: 'Jersey',
-  //Overtops
-  SWST: 'SweatShirt',
-  HOOT: 'Hoodie',
-  BLZT: 'Blazer',
-  //Jackets
-  JAKC: 'Jacket',
-  COAC: "Coat",
-  VESC: "Vest",
-  PUFC: 'Puffer',
-  //Pants
-  SKIP: 'Skirt',
-  YOGP: 'Yoga Pants',
-  CARP: 'Cargo Pants',
-  JEAP: 'Jeans',
-  SWTP: 'Sweatpants',
-  TRAP: 'Track Pants',
-  DRSP: 'Dress Pants',
-  PJMT: 'Pajamas',
-  //shorts
-  RGSH: 'Regular',
-  SWMH: 'Swimshorts',
-  SWSH: 'Sweatshorts',
-  ATSH: 'Athletic Shorts',
-  //Shoes
-  SNEO: 'Sneakers',
-  RUNO: 'Runners',
-  BOTO: 'Boots',
-  DRSO: 'Dress',
-  SNDO: 'Sandals',
-  FLIO: 'Flip Flops',
-  HELO: 'Heels',
-  FLTO: 'Flats',
-  //Hats
-  BENA: 'Beanie',
-  BASA: 'Baseball',
-  SNPA: 'Snapback',
-  BCKA: 'Bucket',
-  VIZA: 'Visor',
-  HATA: 'Regular Hat',
-  //Misc
-  DREX: 'Dress',
-  TUXX: 'Tux'
 
-};
 
 
 function get(set) {
