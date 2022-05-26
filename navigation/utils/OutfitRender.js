714052bdbc8d5206be1df938e8282dcfd5832ca8
Kunde Govindarajan Dhatchinamoorthi <kgdhatchi@Kundes-MacBook-Air.local>
2022-05-25 19:00:00 -0700

    fixed stupid mistake with UI

--- navigation/utils/OutfitRender.js
+++ navigation/utils/OutfitRender.js
@@ -7,8 +7,6 @@
 import { Image, Text, View, StyleSheet } from "react-native";
 import type_mapping from "../../components/type_mapping";
 
-//Local Imports
-import Styles from "../../assets/StyleSheets/OutfitRenderStyle"
 const colors = {
   red: "#EC2379",
   blue: "#0070FF",
@@ -32,9 +30,9 @@ export const Card = ({ card }) => {
   var outfit;
   if (card != null) {
     outfit = card.map((value, idx) => (
-      <View key={idx} style={Styles.item}>
-        <View style={Styles.itemLeft}>
-          <View style={Styles.cardImage}>
+      <View key={idx} style={styles.item}>
+        <View style={styles.itemLeft}>
+          <View style={styles.cardImage}>
             {/* <View key={idx} style={styles.cardImage}> */}
             <Image
               style={{ width: 100, height: 50, paddingTop: 60 }}
@@ -44,7 +42,7 @@ export const Card = ({ card }) => {
                   : require("../../assets/white_item.jpeg")
               }
             />
-            <Text key={idx} style={Styles.itemText}>
+            <Text key={idx} style={styles.itemText}>
               {type_mapping[value.type] + " " + value.color}
             </Text>
             {/* </View> */}
@@ -53,7 +51,7 @@ export const Card = ({ card }) => {
       </View>
     ));
   }
-  return <View style={Styles.card}>{outfit}</View>;
+  return <View style={styles.card}>{outfit}</View>;
 };
 
 /*
@@ -66,8 +64,52 @@ Input : index --> text representation of the card details to be displayed
 
 export const CardDetails = ({ index }) => (
   <View style={{ alignItems: "center" }}>
-    <Text style={[Styles.text, Styles.heading]} numberOfLines={2}>
-      <Text style={[Styles.text, Styles.price]}>{index}</Text>
+    <Text style={[styles.text, styles.heading]} numberOfLines={2}>
+      <Text style={[styles.text, styles.price]}>{index}</Text>
     </Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  cardImage: {
+    width: 160,
+    height: 90,
+    flex: 1,
+    resizeMode: "contain",
+    paddingLeft: 130,
+    paddingTop: 10,
+  },
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingTop: 100,
+    paddingBottom: 100,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
+  },
+  item: {
+    backgroundColor: "white",
+    borderRadius: 7,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+});
